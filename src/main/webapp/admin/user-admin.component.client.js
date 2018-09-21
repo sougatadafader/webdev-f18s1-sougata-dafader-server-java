90c53b5dbbf9b0e66f4a77e7e00652fef1967a61
sougatadafader <sougatadafader@gmail.com>
2018-09-21 17:55:52 -0400

    added json, parsing logic and editing the value

--- src/main/webapp/admin/user-admin.component.client.js
+++ src/main/webapp/admin/user-admin.component.client.js
@@ -1,30 +1,59 @@
 (function () {
-    var $usernameFld, $passwordFld, $emailFld, roleFld; 
-	var $removeBtn, $selectBtn, $updateBtn, $createBtn;
-    var $firstNameFld, $lastNameFld;
-    var $userRowTemplate, $tbody;
+	var $usernameFld, $passwordFld, $emailFld, roleFld; 
+	var $removeBtn, $selectBtn, $updateBtn, $createBtn, $searchBtn;
+	var $firstNameFld, $lastNameFld;
+	var $userRowTemplate, $tbody;
+	var $currentrow;
 
-    var userService = new AdminUserServiceClient();
-   
-    $(main);
+	var userService = new AdminUserServiceClient();
 
-    function main() { 
-    	$usernameFld = $("#usernameFld");
-    	$passwordFld = $("#passwordFld");
-    	$firstNameFld = $("#firstNameFld");
-    	$lastNameFld = $("#lastNameFld");
-    	$emailFld = $("#emailFld");
-    	$roleFld = $("#roleFld");
+	$(main);
 
-    	$userRowTemplate = $(".wbdv-template.wbdv-user");
-    	$tbody = $(".wbdv-tbody");
+	function main() { 
+		$usernameFld = $("#usernameFld");
+		$passwordFld = $("#passwordFld");
+		$firstNameFld = $("#firstNameFld");
+		$lastNameFld = $("#lastNameFld");
+		$emailFld = $("#emailFld");
+		$roleFld = $("#roleFld");
+
+		$userRowTemplate = $(".wbdv-template.wbdv-user");
+		$tbody = $(".wbdv-tbody");
     	//$tbody.append($userRowTemplate.clone());
 
-
+    	$searchBtn = $("#searchBtn");
     	$createBtn = $("#createBtn");
-    	$updateBtn = $("updateBtn");
+    	$updateBtn = $("#updateBtn");
+    	loadUsers();
     	$createBtn.click(createUser);
     	$removeBtn.click(deleteUser);
+    	$selectBtn.click(selectUser);
+    }
+    function loadUsers()
+    {
+    	$.getJSON('../content/users.json',function(json){
+  	var tr;
+  	for (var i = 0; i < json.length; i++) {
+  		tr = $('<tr class="wbdv-template wbdv-user"/>');
+  		tr.append('<td class="wbdv-username">' + json[i].username + '</td>');
+  		tr.append("<td class='wbdv-password'>" + json[i].password + "</td>");
+  		tr.append("<td class='wbdv-email'>" + json[i].email + "</td>");
+  		tr.append("<td class='wbdv-first-name'>" + json[i].firstName + "</td>");
+  		tr.append("<td class='wbdv-last-name'>" + json[i].lastName + "</td>");
+  		tr.append("<td class='wbdv-role'>" + json[i].role + "</td>");
+  		tr.append("<td class='wbdv-actions'><span class='float-right'><i class='fa-2x fa fa-times wbdv-remove'></i><i class='fa-2x fa fa-pencil wbdv-edit'></i></span></td>");
+  		$('table').append(tr);
+  		var timestamp = (new Date().getTime());
+  		tr.find(".wbdv-remove")
+  		.attr("id",timestamp-(i*1000));
+  	}
+  	$('.wbdv-remove').click(function(evt){
+  		deleteUser(evt);
+  	});
+  	$('.wbdv-edit').click(function(evt){
+  		selectUser(evt);
+  	});
+  });
     }
     function createUser() { 
     	var username = $usernameFld.val();
@@ -40,8 +69,8 @@
     	var role = $roleFld.val();
 
     	var timestamp = (new Date().getTime());
-		
-		var newUser = $userRowTemplate.clone();
+
+    	var newUser = $userRowTemplate.clone();
     	
     	/*
     	newUser.removeClass("wbdv-hidden");
@@ -49,37 +78,40 @@
 
     	//	We can also write it as: 
     	newUser
-    		.removeClass("wbdv-hidden")
-    		.find(".wbdv-username")
-    		.html(username);
+    	.removeClass("wbdv-hidden")
+    	.find(".wbdv-username")
+    	.html(username);
     	console.log(username);
     	newUser
-    		.removeClass("wbdv-hidden")
-    		.find(".wbdv-password")
-    		.html(password);
-    		console.log(password);
+    	.removeClass("wbdv-hidden")
+    	.find(".wbdv-password")
+    	.html(password);
+    	console.log(password);
     	newUser
-    		.removeClass("wbdv-hidden")
-    		.find(".wbdv-email")
-    		.html(email);
-    		console.log(email);
+    	.removeClass("wbdv-hidden")
+    	.find(".wbdv-email")
+    	.html(email);
+    	console.log(email);
     	newUser
-    		.removeClass("wbdv-hidden")
-    		.find(".wbdv-first-name")
-    		.html(firstName);
-    		console.log(firstName);
+    	.removeClass("wbdv-hidden")
+    	.find(".wbdv-first-name")
+    	.html(firstName);
+    	console.log(firstName);
     	newUser
-    		.removeClass("wbdv-hidden")
-    		.find(".wbdv-last-name")
-    		.html(lastName);
+    	.removeClass("wbdv-hidden")
+    	.find(".wbdv-last-name")
+    	.html(lastName);
     	newUser
-    		.removeClass("wbdv-hidden")
-    		.find(".wbdv-role")
-    		.html(role);
+    	.removeClass("wbdv-hidden")
+    	.find(".wbdv-role")
+    	.html(role);
     	newUser
-    		.find(".wbdv-remove")
-    		.attr("id",timestamp)
-    		.click(deleteUser)
+    	.find(".wbdv-remove")
+    	.attr("id",timestamp)
+    	.click(deleteUser);
+    	newUser
+    	.find(".wbdv-edit")
+    	.click(selectUser)
 
 
     	
@@ -90,8 +122,7 @@
     }
     function findAllUsers() {  }
     function findUserById() {  }
-    function deleteUser(event) { 
-    	
+    function deleteUser(event) {
     	/*
     	Finds the ID
     	var id= $(event.currentTarget).attr("id");
@@ -101,9 +132,33 @@
     	var tr =  button.parents(".wbdv-template");
     	console.log(tr);
     	tr.remove();
+    }
+    function selectUser() { 
+    	var button = $(event.currentTarget);
+    	var current_tr =  button.parents(".wbdv-template.wbdv-user");
+    	currentrow = current_tr.find(".wbdv-remove").attr("id");
+    	//alert(currentrow);
+    	var curr_username = current_tr.find('.wbdv-username').text();
+    	$('#usernameFld').val(curr_username);
+    	var curr_password = current_tr.find('.wbdv-password').text();
+    	$('#passwordFld').val(curr_password);
+    	var curr_email = current_tr.find('.wbdv-email').text();
+    	$('#emailFld').val(curr_email);
+    	var curr_first_name = current_tr.find('.wbdv-first-name').text();
+    	$('#firstNameFld').val(curr_first_name);
+    	var curr_last_name = current_tr.find('.wbdv-last-name').text();
+    	$('#lastNameFld').val(curr_last_name);
+    	var curr_role = current_tr.find('.wbdv-role').text();
+    	$('#roleFld').val(curr_role);
 
-     }
-    function selectUser() {  }
+
+
+
+
+
+    	//var 
+
+    }
     function updateUser() {  }
     function renderUser(user) {  }
     function renderUsers(users) {  }
